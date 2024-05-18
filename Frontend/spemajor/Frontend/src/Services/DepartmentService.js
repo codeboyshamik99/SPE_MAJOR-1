1fdaca42be4944fb788528baacfc30383e89cfa7
codeboyshamik99 <121502383+codeboyshamik99@users.noreply.github.com>
2024-05-18 18:38:56 +0530

    Update DepartmentService.js

--- Frontend/spemajor/Frontend/src/Services/DepartmentService.js
+++ Frontend/spemajor/Frontend/src/Services/DepartmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const DEPARTMENT_REST_API_BASE_URL = 'http://localhost:8080/api/departments'
+const DEPARTMENT_REST_API_BASE_URL = 'http://localhost:8090/api/departments'
 
 export const getAllDepartments = () => axios.get(DEPARTMENT_REST_API_BASE_URL);
 
